ade2a95b3af1308fd43519015d697add10a4c678
Dmitriy Bulakhov <bulakhovda@gmail.com>
2023-03-22 12:13:55 +0200

    Created Paper for operations content

--- src/pages/Operations/Operations.jsx
+++ src/pages/Operations/Operations.jsx
@@ -1,9 +1,21 @@
+import { Paper } from '@mui/material';
 import OperationsTable from 'components/OperationsTable/OperationsTable';
 
 export default function Home() {
   return (
     <div>
-      <OperationsTable />
+      <Paper
+        sx={{
+          margin: '30px',
+          maxWidth: 1100,
+          maxHeight: 580,
+          borderRadius: '0px 30px 30px 30px',
+          boxShadow: '0px 10px 60px rgba(170, 178, 197, 0.2)',
+          padding: '32px 32px 61px 32px',
+        }}
+      >
+        <OperationsTable />
+      </Paper>
     </div>
   );
 }
