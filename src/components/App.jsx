ce028c645be6b149cdbe96c6c697295f81e0d257
Oleg Kropiviansky <oleg.kropiviansky@gmail.com>
2023-03-22 20:18:53 +0200

    fix styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,24 @@
-import { lazy } from 'react';
-import { Route, Routes } from 'react-router-dom';
+// import { lazy } from 'react';
+// import { Route, Routes } from 'react-router-dom';
 // import { PrivateRoute } from 'routes/PrivateRoutes';
 // import { RestrictedRoute } from 'routes/RestrictedRoute';
-import Layout from 'pages/Layout/Layout';
+// import Layout from 'pages/Layout/Layout';
+import Header from './Header/Header.jsx';
 
-const AuthPage = lazy(() => import('../pages/AuthPage/AuthPage.jsx'));
-const Operations = lazy(() => import('../pages/Operations/Operations'));
-const Report = lazy(() => import('../pages/Report/Report'));
+// const AuthPage = lazy(() => import('../pages/AuthPage/AuthPage.jsx'));
+// const Operations = lazy(() => import('../pages/Operations/Operations'));
+// const Report = lazy(() => import('../pages/Report/Report'));
 export const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<AuthPage />} />
-        <Route path="/operations" element={<Operations />} />
-        <Route path="/report" element={<Report />} />
-        <Route path="*" element={<div>page not found</div>} />
-      </Route>
-    </Routes>
+    <Header />
+
+    // <Routes>
+    //   <Route path="/" element={<Layout />}>
+    //     <Route index element={<AuthPage />} />
+    //     <Route path="/operations" element={<Operations />} />
+    //     <Route path="/report" element={<Report />} />
+    //     <Route path="*" element={<div>page not found</div>} />
+    //   </Route>
+    // </Routes>
   );
 };
