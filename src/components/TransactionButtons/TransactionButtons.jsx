ea392cb298de9a9f322fba96a369cc6e6cbd42b6
Daniel <daniil.spectr@gmail.com>
2023-03-27 02:36:00 +0300

    add wrapp in buttons

--- src/components/TransactionButtons/TransactionButtons.jsx
+++ src/components/TransactionButtons/TransactionButtons.jsx
@@ -1,16 +1,14 @@
-
-import {  LinkButton, Buttons } from './TransactionsButton.styled'
-
+import { LinkButton, Buttons, Wrapper } from './TransactionsButton.styled';
 
 const TransactionButtons = () => {
-      
-      return (
-            <><Buttons>
-                  <LinkButton to="/" >Expenses</LinkButton>
-            
-                  <LinkButton to="/income "  >Income</LinkButton>
-                  </Buttons>
-                  </>
-     )
-}
+  return (
+    <Wrapper>
+      <Buttons>
+        <LinkButton to="/">Expenses</LinkButton>
+
+        <LinkButton to="/income ">Income</LinkButton>
+      </Buttons>
+    </Wrapper>
+  );
+};
 export default TransactionButtons;
